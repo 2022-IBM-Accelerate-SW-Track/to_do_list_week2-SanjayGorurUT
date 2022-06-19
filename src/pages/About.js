54d9de8fe2390ea0c2aa332ae19c3e85b5f850b5
sanjaygorur <sanjaygorur@bitbucket.org>
2022-06-18 19:10:04 -0500

    All tests passed

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,35 @@
-import React, { Component } from "react";
-import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import React, { Component } from 'react';
 
+import "./About.css";
+import sanjaygorur from "../../src/assets/sanjaygorur.jpg";
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
-        </div>
+          <p>      
+            <div>
+              <div class="split left">
+                <div className="centered">
+                  <img 
+                    className="profile_image"
+                    src={sanjaygorur}
+                    alt="Profile Pic"
+                    ></img>
+                </div>
+              </div>
+              <div className="split right">
+                <div className="centered">
+                  <div className="name_title">Sanjay Gorur</div>
+                  <div className="brief_description">
+                    Hi, my name is Sanjay, and I'm an undergraduate at UT Austin. My hobbies include basketball, chess, and football. <n> </n> My dream career
+                    is software engineering, and I think this program gives me a great opportunity to pursue my passion(s).
+                  </div>
+                </div>
+              </div>
+        </div> 
+        </p> 
       </div>
-    );
+    )
   }
 }
